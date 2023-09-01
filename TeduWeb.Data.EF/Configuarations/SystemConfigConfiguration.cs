6f9084bd261bb175ccc9cb8b7828b65e52e06e0c
Nolan <tranfc911@gmail.com>
2023-09-01 22:07:29 +0700

    Full Interface

---
+++ TeduWeb.Data.EF/Configuarations/SystemConfigConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeduWeb.Data.EF.Extensions;
+using TeduWeb.Data.Entities;
+
+namespace TeduWeb.Data.EF.Configurations
+{
+    class SystemConfigConfiguration : DbEntityConfiguration<SystemConfig>
+    {
+        public override void Configure(EntityTypeBuilder<SystemConfig> entity)
+        {
+            entity.Property(c => c.Id).HasMaxLength(255).IsRequired();
+            // etc.
+        }
+    }
+}
